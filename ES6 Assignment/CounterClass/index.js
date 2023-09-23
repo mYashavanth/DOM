2dada7c8cc3604b1a6fc476537b2dec0440f2fab
mYashavanth <yashavantham143@gmail.com>
2023-09-23 20:59:16 +0530

    added ES6 assignment

--- ES6 Assignment/CounterClass/index.js
+++ ES6 Assignment/CounterClass/index.js
@@ -1,52 +1,46 @@
-let startTime = 0;
-let setTime;
-
 class countdown{
-    constructor(){}
-    start(){
-        setTime= setInterval(()=>{
-            startTime++;
-            document.querySelector("h1").innerText = startTime;
-            // console.log(startTime);
+    constructor(){
+        this.startTime = 0;
+        this.setTime;
+    }
+    start=()=>{
+        this.setTime = setInterval(()=>{
+            this.startTime++;
+            document.querySelector("h1").innerText = this.startTime;
         },1000)
     }
-}
-let counter = new countdown()
-document.querySelector("#start").addEventListener("click",startStop)
-document.querySelector("#reset").addEventListener("click",reset)
-document.querySelector("#increment").addEventListener("click",increment)
-document.querySelector("#decrement").addEventListener("click",decrement)
-
-function stop(){
-    clearInterval(setTime)
-    setTime=null;
-}
-function startStop(){
-    if(!setTime){
-        counter.start()
-        document.querySelector("#start").innerText = "Stop"
-    }else{
-        stop()
+    stop=()=>{
+        clearInterval(this.setTime)
+        this.setTime = null;
+    }
+    startStop=()=>{
+        if(!this.setTime){
+            this.start()
+            document.querySelector("#start").innerText = "Stop"
+        }else{
+            this.stop()
+            document.querySelector("#start").innerText = "Start"
+        }
+    }
+    increment=()=>{
+        this.stop()
+        this.startTime++
+        document.querySelector("h1").innerText = this.startTime;
+    }
+    decrement=()=>{
+        this.stop()
+        this.startTime--
+        document.querySelector("h1").innerText = this.startTime;
+    }
+    reset=()=>{
+        this.stop()
+        this.startTime = 0;
+        document.querySelector("h1").innerText = 0;
         document.querySelector("#start").innerText = "Start"
     }
 }
-function increment(){
-    stop()
-    startTime++
-    document.querySelector("h1").innerText = startTime;
-}
-function decrement(){
-    stop()
-    startTime--
-    document.querySelector("h1").innerText = startTime;
-}
-function reset(){
-    stop();
-    startTime = 0;
-    document.querySelector("h1").innerText = 0;
-    document.querySelector("#start").innerText = "Start"
-}
-
-
-
- 
+let counter = new countdown()
+document.querySelector("#start").addEventListener("click",counter.startStop)
+document.querySelector("#reset").addEventListener("click",counter.reset)
+document.querySelector("#increment").addEventListener("click",counter.increment)
+document.querySelector("#decrement").addEventListener("click",counter.decrement)
