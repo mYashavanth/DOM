ef1d9e27fe21bf5cf59590f24dd3830219c32a36
mYashavanth <yashavantham143@gmail.com>
2023-09-24 12:19:00 +0530

    added omdb assignment

--- FetchAssignment/OmdbMovieApp/index.js
+++ FetchAssignment/OmdbMovieApp/index.js
@@ -23,39 +23,39 @@ function displayData(data){
     document.querySelector("#cont").innerHTML="";
     if(data){
         data.forEach(function(ele){
-            // let img = document.createElement("img");
-            // img.src = ele.Poster;
+            let img = document.createElement("img");
+            img.src = ele.Poster;
     
-            // let title = document.createElement("h2");
-            // title.innerText = `Title: ${ele.Title}`;
+            let title = document.createElement("h2");
+            title.innerText = `Title: ${ele.Title}`;
             
-            // let imdbID = document.createElement("h4");
-            // imdbID.innerText = `imdbID: ${ele.imdbID}`;
+            let imdbID = document.createElement("h4");
+            imdbID.innerText = `imdbID: ${ele.imdbID}`;
             
-            // let type = document.createElement("h4");
-            // type.innerText = `Type: ${ele.Type}`;
+            let type = document.createElement("h4");
+            type.innerText = `Type: ${ele.Type}`;
     
-            // let year = document.createElement("p");
-            // year.innerText = `Year: ${ele.Year}`;
+            let year = document.createElement("p");
+            year.innerText = `Year: ${ele.Year}`;
     
-            // let div = document.createElement("div");
-            // div.className = "card"
+            let div = document.createElement("div");
+            div.className = "card"
     
-            // div.append(img,title,imdbID,type,year)
-            // document.querySelector("#cont").append(div);
+            div.append(img,title,imdbID,type,year)
+            document.querySelector("#cont").append(div);
 
-            // div.addEventListener("click",function(){
-            //     localStorage.setItem("imdbID",ele.imdbID);
-            //     location.href = "index2.html"
-            // })
-            document.querySelector("#cont").innerHTML += 
-            `<div class="card">
-            <img src="${ele.Poster}" alt="">
-            <h2>${ele.Title}</h2>
-            <h4>${ele.imdbID}</h4>
-            <h4>${ele.Type}</h4>
-            <p>${ele.Year}</p>
-            </div>`
+            div.addEventListener("click",function(){
+                localStorage.setItem("imdbID",ele.imdbID);
+                location.href = "index2.html"
+            })
+            // document.querySelector("#cont").innerHTML += 
+            // `<div class="card">
+            // <img src="${ele.Poster}" alt="">
+            // <h2>${ele.Title}</h2>
+            // <h4>${ele.imdbID}</h4>
+            // <h4>${ele.Type}</h4>
+            // <p>${ele.Year}</p>
+            // </div>`
         })
     }else{
         let title = document.createElement("h2");
